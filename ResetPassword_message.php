92041ae7aff90136a8fabb7273121c79e5e4b2dd
GitHubMinhNhat <103959512+GitHubMinhNhat@users.noreply.github.com>
2023-03-08 16:32:51 +0700

    Update ResetPassword_message.php

--- ResetPassword_message.php
+++ ResetPassword_message.php
@@ -3,7 +3,7 @@
 <?php 
     require_once __DIR__. "/autoload/autoload.php"; 
 ?>
-
+<!-- This is HEADER -->
 <?php require_once __DIR__. "/layouts/header.php" ;?>
 <?php require_once __DIR__. "/layouts/banner.php" ;?>
 <!-- END HEADER -->
